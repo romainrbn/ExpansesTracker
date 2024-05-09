18c20287ab6e551ffb2c342f4815d51e594826bd
Romain Rabouan <romain.rabouan@withings.com>
2024-05-09 06:39:06 +0200

    Initial Commit

---
+++ AIExpansesTracker/AIExpansesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIExpansesTrackerApp.swift
+//  AIExpansesTracker
+//
+//  Created by Romain Rabouan on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AIExpansesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
